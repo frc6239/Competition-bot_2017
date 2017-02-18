65853ceba0c79c78e46c7a9d79089efd3424b742
ThemadGrammer <blueberryman2112@gmail.com>
2017-02-17 21:42:23 -0500

    timer

--- src/org/usfirst/frc/team6239/robot/subsystems/VisionSub.java
+++ src/org/usfirst/frc/team6239/robot/subsystems/VisionSub.java
@@ -30,17 +30,12 @@ public class VisionSub extends Subsystem {
 			
 			if (drive_config.equals(DriveConfig.left)) {
 				
-				while (30 > Robot.robotmap.curAngle + 10 && Robot.robotmap.isSeen) {
+				while (20 > Robot.robotmap.curAngle && Robot.robotmap.isSeen) {
 					Robot.DRIVE_SUB.TurnLeft(1, 1);
 					
 					if (Robot.robotmap.curAngle > 20 && Robot.robotmap.curAngle < 20.5) {
 						Robot.DRIVE_SUB.tank(1, 1);
-						try {
-							time.wait(1);
-						} catch (InterruptedException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
+						time.delay(1.0);
 					}
 					
 					
@@ -49,17 +44,12 @@ public class VisionSub extends Subsystem {
 				
 			} else if (drive_config.equals(DriveConfig.right)) {
 				
-				while (-30 > Robot.robotmap.curAngle + 10 && Robot.robotmap.isSeen) {
+				while (-20 > Robot.robotmap.curAngle && Robot.robotmap.isSeen) {
 					Robot.DRIVE_SUB.TurnRight(1, 1);
 					
 					if (Robot.robotmap.curAngle > 20 && Robot.robotmap.curAngle < 20.5) {
 						Robot.DRIVE_SUB.tank(1, 1);
-						try {
-							time.wait(1);
-						} catch (InterruptedException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
+						time.delay(1.0);
 					}
 					
 					
